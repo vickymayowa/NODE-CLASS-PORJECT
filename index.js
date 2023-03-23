60d73c43d30d8152fc2e79c5edbe106a46bc0097
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-03-23 16:52:13 +0100

    i added some post request and get request to my ejs files

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const ejs = require('ejs')
 app.set("view engine", "ejs")
 
  app.get("/",(req,res)=>{
-    console.log("Request Made")
-   console.log(__dirname)
-   res.sendFile(__dirname + "/index.html")
+  //   console.log("Request Made")
+  //  console.log(__dirname)
+   res.render("index")
  })
 
  app.listen(5000,()=>{
@@ -20,10 +20,24 @@ app.set("view engine", "ejs")
     console.log("Your server is live Now")
  })
  app.get("/index",(req,res)=>{
-   res.render("index")
+   res.render("SignIn")
  })
- app.get("/vicky",(req,res)=>{
-   res.render("vicky")
-   console.log("hello world", {name:"Vicky", school:"SQI"})
+//  app.get("/vicky",(req,res)=>{
+//    res.render("SignIn")
+//   //  console.log("hello world", {name:"Vicky", school:"SQI"})
+//  })
+ app.get("/SignIn",(req,res)=>{
+  res.render("SignIn")
  })
-console.log("hello world")
+ app.post("/vicky", (req, res) => {
+  res.render("index")
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Authenticate the user's credentials
+  if (username === "vicky" && password === "password") {
+    res.send("Login successful!");
+  } else {
+    res.send("Invalid username or password.");
+  }
+});
