8c36de115b83c7f6b12f7f44f55f6d23cf662e74
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-04-04 11:18:19 +0100

    i added the database schema

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const {req,res} = require('express');
+const mongoose = require("mongoose")
 const express = require('express')
 const app = express();
 const ejs = require('ejs'); 
 const bodyParser = require('body-parser');
+const URI = "mongodb+srv://favouradebanjo603:adebanjo123456cluster0.ae47tji.mongodb.net/nodeclass_db?retryWrites=true&w=majority"
+
+mongoose.connect(URI)
+.then(()=>{
+  console.log("Mongoose HandShake Approved");
+})
+.catch((err)=>{
+  console.log("Mongoose Handshake Disconnected");
+  console.log(err);
+})
+
+
 app.set("view engine", "ejs")
 
  app.get("/",(req, res)=>{
